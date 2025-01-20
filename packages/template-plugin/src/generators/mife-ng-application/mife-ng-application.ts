aba65e0e02903b84ce6ae359536b7470427c7dab
imrih <imrih@infraedge.co.il>
2025-01-20 17:29:05 +0200

    generator refactor

--- packages/template-plugin/src/generators/mife-ng-application/mife-ng-application.ts
+++ packages/template-plugin/src/generators/mife-ng-application/mife-ng-application.ts
@@ -1,11 +1,57 @@
-import { formatFiles, generateFiles, Tree, names } from '@nx/devkit';
+import { formatFiles, generateFiles, Tree, names, readJson, writeJson } from '@nx/devkit';
 import * as path from 'path';
+import { angularInitGenerator } from '@nx/angular/generators';
 import { MifeNgApplicationGeneratorSchema } from './schema';
 
-export async function mifeNgApplicationGenerator(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
-  const templateNames = names(options.name)
+function addFiles(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
+  const templateNames = names(options.name);
   const projectRoot = `${options.directory}/${templateNames.fileName}`;
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {template: '', ...templateNames, ...options});
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    template: '',
+  };
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, templateOptions);
+}
+
+function updateJsonFile(tree: Tree, filePath: string, key: string, value: string) {
+  if (!tree.exists(filePath)) {
+    tree.write(filePath, JSON.stringify({}, null, 2));
+  }
+  try {
+    const json = readJson(tree, filePath);
+    json[key] = value;
+    writeJson(tree, filePath, json);
+  } catch (error) {
+    console.error(`Error updating ${filePath}:`, error);
+    throw new Error(`Failed to update JSON at ${filePath}`);
+  }
+}
+
+function updateImportmapMfJson(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
+  const templateNames = names(options.name);
+  const filePath = 'apps/app-shell/public/importmaps/importmap-mf.prod.json';
+  const key = `@myorg/${templateNames.fileName}`;
+  const value = `./mf/${templateNames.fileName}/main.js`;
+
+  updateJsonFile(tree, filePath, key, value);
+}
+
+function updateImportmapMfProdJson(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
+  const templateNames = names(options.name);
+  const filePath = 'apps/app-shell/public/importmaps/importmap-mf.json';
+  const key = `@myorg/${templateNames.fileName}`;
+  const value = `http://localhost:${options.port}/main.js`;
+
+  updateJsonFile(tree, filePath, key, value);
+}
+
+export async function mifeNgApplicationGenerator(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
+  angularInitGenerator(tree, { skipFormat: true });
+  addFiles(tree, options);
+  updateImportmapMfJson(tree, options);
+  updateImportmapMfProdJson(tree, options);
   await formatFiles(tree);
 }
 
