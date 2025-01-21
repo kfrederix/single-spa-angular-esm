2e8db0f8e8443e2b9285868c54101cc9473d6200
imrih <imrih@infraedge.co.il>
2025-01-21 17:22:04 +0200

    refactor all generator generate strategy

--- packages/template-plugin/src/generators/mife-ng-application/mife-ng-application.ts
+++ packages/template-plugin/src/generators/mife-ng-application/mife-ng-application.ts
@@ -1,57 +1,85 @@
-import { formatFiles, generateFiles, Tree, names, readJson, writeJson } from '@nx/devkit';
-import * as path from 'path';
-import { angularInitGenerator } from '@nx/angular/generators';
+import { formatFiles, Tree, names, getProjects } from '@nx/devkit';
+import { applicationGenerator } from '@nx/angular/generators';
 import { MifeNgApplicationGeneratorSchema } from './schema';
-
-function addFiles(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
-  const templateNames = names(options.name);
-  const projectRoot = `${options.directory}/${templateNames.fileName}`;
-  const templateOptions = {
-    ...options,
-    ...names(options.name),
-    template: '',
-  };
-
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, templateOptions);
-}
-
-function updateJsonFile(tree: Tree, filePath: string, key: string, value: string) {
-  if (!tree.exists(filePath)) {
-    tree.write(filePath, JSON.stringify({}, null, 2));
-  }
-  try {
-    const json = readJson(tree, filePath);
-    json[key] = value;
-    writeJson(tree, filePath, json);
-  } catch (error) {
-    console.error(`Error updating ${filePath}:`, error);
-    throw new Error(`Failed to update JSON at ${filePath}`);
-  }
-}
+import { deleteFolderOrFile } from '../../utils/delete-folder';
+import { updateTsConfigType, updateTsConfigCompileOption } from '../../utils/add-vite-types-config';
+import { updateImportmapJson } from '../../utils/importmaps-helpers';
+import { updateProjectJson } from '../../utils/update-project';
+import { addFiles } from '../../utils/add-files';
+import path from 'path';
 
 function updateImportmapMfJson(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
   const templateNames = names(options.name);
   const filePath = 'apps/app-shell/public/importmaps/importmap-mf.prod.json';
-  const key = `@myorg/${templateNames.fileName}`;
-  const value = `./mf/${templateNames.fileName}/main.js`;
-
-  updateJsonFile(tree, filePath, key, value);
+  const importmap: { [key: string]: string } = {
+    [`@myorg/${templateNames.fileName}`]: `./mf/${templateNames.fileName}/main.js`,
+  };
+  updateImportmapJson(tree, filePath, importmap);
 }
 
 function updateImportmapMfProdJson(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
   const templateNames = names(options.name);
   const filePath = 'apps/app-shell/public/importmaps/importmap-mf.json';
-  const key = `@myorg/${templateNames.fileName}`;
-  const value = `http://localhost:${options.port}/main.js`;
-
-  updateJsonFile(tree, filePath, key, value);
+  const importmap: { [key: string]: string } = {
+    [`@myorg/${templateNames.fileName}`]: `http://localhost:${options.port}/main.js`,
+  };
+  updateImportmapJson(tree, filePath, importmap);
 }
 
 export async function mifeNgApplicationGenerator(tree: Tree, options: MifeNgApplicationGeneratorSchema) {
-  angularInitGenerator(tree, { skipFormat: true });
-  addFiles(tree, options);
+  //generator consts
+  const serializedNames = names(options.name);
+  const serializedDirectoryNames = names(options.directory);
+  const projects = getProjects(tree);
+
+  //dir consts
+  const appDir = `${serializedDirectoryNames.fileName}/${serializedNames.fileName}`;
+  const tsConfigAppPath = `${appDir}/tsconfig.app.json`;
+  const tsConfigEditorPath = `${appDir}/tsconfig.editor.json`;
+  const tsConfigRootPath = `${appDir}/tsconfig.json`;
+  const projectJsonPath = `${appDir}/project.json`;
+
+  if (projects.has(serializedNames.fileName)) {
+    throw new Error(`Project '${serializedNames.fileName}' already exists.`);
+  }
+
+  //generate angular application
+  await applicationGenerator(tree, {
+    name: serializedNames.fileName,
+    directory: `${serializedDirectoryNames.fileName}/${serializedNames.fileName}`,
+    prefix: serializedNames.fileName,
+    bundler: 'esbuild',
+    port: options.port,
+    skipTests: options.skipTests,
+    addTailwind: options.addTailwind,
+    style: options.style,
+    unitTestRunner: options.unitTestRunner,
+    e2eTestRunner: options.e2eTestRunner,
+    linter: options.linter,
+    tags: options.tags,
+  });
+
+  //delete unnecessary files/dirs
+  deleteFolderOrFile(tree, `${appDir}/src/app`);
+  deleteFolderOrFile(tree, `${appDir}/src/index.html`);
+  deleteFolderOrFile(tree, `${appDir}/src/main.ts`);
+  deleteFolderOrFile(tree, `${appDir}/public`);
+
+  //add files from template
+  addFiles(tree, path.join(__dirname, 'files'), options);
+
+  //update tsconfig files to fit the template
+  updateTsConfigType(tree, tsConfigAppPath, ['vite/client']);
+  updateTsConfigType(tree, tsConfigEditorPath, ['vite/client']);
+  updateTsConfigCompileOption(tree, tsConfigRootPath, {}, true);
+
+  //update the app-shell importmaps files
   updateImportmapMfJson(tree, options);
   updateImportmapMfProdJson(tree, options);
+
+  //update project.json
+  updateProjectJson(tree, options, projectJsonPath);
+
   await formatFiles(tree);
 }
 
