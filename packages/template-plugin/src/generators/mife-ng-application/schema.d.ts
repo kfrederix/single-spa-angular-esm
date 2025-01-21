cca537eb93e881a84cc7ac8aac7d17bfd15ced37
imrih <imrih@infraedge.co.il>
2025-01-21 17:21:29 +0200

    update schema

--- packages/template-plugin/src/generators/mife-ng-application/schema.d.ts
+++ packages/template-plugin/src/generators/mife-ng-application/schema.d.ts
@@ -1,5 +1,16 @@
+import { E2eTestRunner, UnitTestRunner } from "@nx/angular/generators";
+import { Styles } from "node_modules/@nx/angular/src/generators/utils/types";
+import type { Linter, LinterType } from '@nx/eslint';
+
 export interface MifeNgApplicationGeneratorSchema {
   name: string;
   directory: string;
   port: number;
+  style?: Styles
+  skipTests?: boolean
+  unitTestRunner?: UnitTestRunner
+  e2eTestRunner?: E2eTestRunner
+  tags?: string;
+  linter?: Linter | LinterType;
+  addTailwind?: boolean
 }
